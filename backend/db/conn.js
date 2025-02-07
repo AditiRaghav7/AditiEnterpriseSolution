8f2713c44e841341b436813c4b62d8f9b75eea0a
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-07 16:08:04 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-host: "my-mysql-container", //public ip of my vm
+  host: "my-mysql-container", //public ip of my vm
   user: "root",
   password: "Aditi@1122",
   port: 3306,   
