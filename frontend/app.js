f568b68a8ebdc76179498075447a1422b89bd9cb
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-10 17:21:13 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const routes = require('./routes')
 var cors = require('cors')
-const connection = require('./db/conn');  
 const app = express();
 
 app.use(cors())
