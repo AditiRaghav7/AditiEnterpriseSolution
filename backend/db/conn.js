061fc6851c3ad8e3768ab4a7e807f6ac0a755ba9
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-10 14:52:13 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,21 +1,19 @@
 const mysql = require("mysql2");
 
+
 // Create a connection to the database
 const connection = mysql.createConnection({
-host: "16.170.223.140",
+  host: "16.170.223.140",
   user: "root",
-  password: "Aditi@1122",
-  port: 3306,   
+  password: "jagriti@123",
+  port: "3306",
   database: "employees_db"
 });
 
 // open the MySQL connection
-connection.connect((error) => {
-  if (error) {
-    console.error("Error connecting to the database:", error.message);  // Log more detailed error
-    process.exit(1);  // Exit if connection fails
-  }
-  console.log("Successfully connected to the MySQL database.");
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the MYSQL database.");
 });
 
 module.exports = connection;
