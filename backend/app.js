75e13636fc84c79f331804fd4cd3a6975583e8ab
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-10 16:22:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,24 +3,22 @@ const appRoutes = require('./routes/appRoutes')
 const bodyParser = require('body-parser');
 const cors = require('cors')
 // const cors = require("cors");
+const connection = require('./db/conn');  // Make sure to import the connection here
+
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
-// const corsOptions = {
-//   origin: "http://localhost:8000"
-// };
+const PORT = process.env.PORT || 8000;
 
-// app.use(cors(corsOptions));
-
-const PORT= process.env.PORT || 8000
-
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", 'ejs')
-app.set('views', 'views')
-app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", appRoutes )
+app.use("/", appRoutes);
 
-app.listen(PORT, ()=> {console.log(`Server is listening on port ${PORT}`);})
+app.listen(PORT,"0.0.0.0",() => {
+  console.log(`Server is listening on port ${PORT}`);
+});
