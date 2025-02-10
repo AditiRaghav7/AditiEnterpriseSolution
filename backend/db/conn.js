26654cbbb8fdbbfc3e8f96cd88698daec64ad67a
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-10 18:42:45 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,4 @@
 const mysql = require("mysql2");
-
-
-// Create a connection to the database
 const connection = mysql.createConnection({
   host: "16.16.201.88",
   user: "root",
@@ -9,11 +6,9 @@ const connection = mysql.createConnection({
   port: "3306",
   database: "employees_db"
 });
-
 // open the MySQL connection
 connection.connect(error => {
   if (error) throw error;
   console.log("Successfully connected to the MYSQL database.");
 });
-
 module.exports = connection;
