c675573b90684302da9cafc3fcfc8cb30e44f491
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-12 10:50:47 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql2");
 
+// Create a connection to the database
 const connection = mysql.createConnection({
-  host: "13.53.119.4", // ✅ Use Docker container name
+  host: "mysql",
   user: "root",
   password: "Aditi@1122",
-  port: "3306",
+  port: 3306,   
   database: "employees_db"
 });
 
-// Open the MySQL connection
-connection.connect(error => {
+// open the MySQL connection
+connection.connect((error) => {
   if (error) {
-    console.error("MySQL Connection Failed:", error.message); // Log error
-    process.exit(1); // Exit process if connection fails
+    console.error("Error connecting to the database:", error.message);  // Log more detailed error
+    process.exit(1);  // Exit if connection fails
   }
-  console.log("Successfully connected to the MYSQL database.");
+  console.log("Successfully connected to the MySQL database.");
 });
 
 module.exports = connection;
